100e797f15af3820648ddf96d75772ecd734ec3e
Johnson <linghuchongxajh@qq.com>
2025-06-10 20:54:07 +0800

    2025-06-10_20:54

--- A2Aclient.py
+++ A2Aclient.py
@@ -1,10 +1,12 @@
-import random
-import time
-import json
 import asyncio
+import base64
+import os
 import urllib
-from uuid import uuid4
 import httpx
+import time
+
+from uuid import uuid4
+
 from a2a.client import A2AClient, A2ACardResolver
 from a2a.types import (
     Part,
@@ -24,161 +26,206 @@ from a2a.types import (
     TaskQueryParams,
     JSONRPCErrorResponse,
 )
-# from common.types import Task, TextPart, FilePart, FileContent # 如有需要可以加上
-from typing import AsyncIterator
 
 
-class A2AClientWrapper:
-    def __init__(self,
-                 session_id,
-                 agent_url="http://localhost:10006",
-                 push_notification_receiver="http://localhost:5000",
-                 use_push_notifications=False,
-                 use_history=False):
-        self.agent_url = agent_url
-        self.use_push_notifications = use_push_notifications
-        self.use_history = use_history
-        self.push_notification_receiver = push_notification_receiver
-        self.session_id = session_id
-        self.client = None
-        self.streaming = False
-        self.notification_receiver_host = None
-        self.notification_receiver_port = None
+async def cli(
+    agent,
+    session,
+    history,
+    use_push_notifications: bool,
+    push_notification_receiver: str,
+    header,
+    prompt,
+    file_path,
+):
+    headers = {h.split("=")[0]: h.split("=")[1] for h in header}
+    print(f"Will use headers: {headers}")
+    async with httpx.AsyncClient(timeout=30, headers=headers) as httpx_client:
+        card_resolver = A2ACardResolver(httpx_client, agent)
+        card = await card_resolver.get_agent_card()
 
-    async def setup(self):
-        async with httpx.AsyncClient(timeout=30, headers={}) as httpx_client:
-            resolver = A2ACardResolver(httpx_client, self.agent_url)
-            card = await resolver.get_agent_card()
-            print("======= Agent Card ========")
-            print(card.model_dump_json(exclude_none=True))
-            self.client = A2AClient(httpx_client, agent_card=card)
-            self.streaming = card.capabilities.streaming
-            notif_receiver_parsed = urllib.parse.urlparse(self.push_notification_receiver)
-            self.notification_receiver_host = notif_receiver_parsed.hostname
-            self.notification_receiver_port = notif_receiver_parsed.port
+        print("======= Agent Card ========")
+        print(card.model_dump_json(exclude_none=True))
 
-    async def run(self, prompt: str):
-        if self.client is None:
-            await self.setup()
+        notif_receiver_parsed = urllib.parse.urlparse(push_notification_receiver)
+        notification_receiver_host = notif_receiver_parsed.hostname
+        notification_receiver_port = notif_receiver_parsed.port
 
-        task_id = uuid4().hex
+        client = A2AClient(httpx_client, agent_card=card)
+
+        streaming = card.capabilities.streaming
+        context_id = session if session > 0 else uuid4().hex
+
         print("=========  starting a new task ======== ")
-        stream_response = self.generate(messages=[{"role": "user", "content": prompt}])
-        async for result in stream_response:
-            print(f"stream event => {result}")
+        taskId = uuid4().hex
+        await completeTask(
+            client,
+            streaming,
+            use_push_notifications,
+            notification_receiver_host,
+            notification_receiver_port,
+            taskId,
+            context_id,
+            prompt,
+            file_path,
+        )
 
 
 
-    async def generate(self, messages: list[dict]) -> AsyncIterator[dict]:
-        """SSE 流式生成器"""
-        if self.client is None:
-            await self.setup()
-        task_id = uuid4().hex
-        prompt = messages[-1]["content"]
-        print(f"发送的问题是: {prompt}")
-        message = {
-            "role": "user",
-            "parts": [
-                {
-                    "type": "text",
-                    "text": prompt,
-                }
-            ]
+async def completeTask(
+    client: A2AClient,
+    streaming,
+    use_push_notifications: bool,
+    notification_receiver_host: str,
+    notification_receiver_port: int,
+    taskId,
+    contextId,
+    prompt: str,
+    file_path: str,
+):
+    if prompt == ":q" or prompt == "quit":
+        return False, None, None
+
+    message = Message(
+        role="user",
+        parts=[TextPart(text=prompt)],
+        messageId=str(uuid4()),
+        taskId=taskId,
+        contextId=contextId,
+    )
+
+    if file_path and file_path.strip() != "":
+        with open(file_path, "rb") as f:
+            file_content = base64.b64encode(f.read()).decode("utf-8")
+            file_name = os.path.basename(file_path)
+
+        message.parts.append(
+            Part(root=FilePart(file=FileWithBytes(name=file_name, bytes=file_content)))
+        )
+
+    payload = MessageSendParams(
+        id=str(uuid4()),
+        message=message,
+        configuration=MessageSendConfiguration(
+            acceptedOutputModes=["text"],
+        ),
+    )
+
+    if use_push_notifications:
+        payload["pushNotification"] = {
+            "url": f"http://{notification_receiver_host}:{notification_receiver_port}/notify",
+            "authentication": {
+                "schemes": ["bearer"],
+            },
         }
-        payload = {
-            "id": task_id,
-            "sessionId": self.session_id,
-            "acceptedOutputModes": ["text"],
-            "message": message,
-        }
 
-        # 如果支持streaming，就一边收，一边yield
-        response_stream = self.client.send_message_streaming(payload)
+    taskResult = None
+    message = None
+    if streaming:
+        response_stream = client.send_message_streaming(
+            SendStreamingMessageRequest(
+                id=str(uuid4()),
+                params=payload,
+            )
+        )
         async for result in response_stream:
-            print(f"收到的结果信息是: {result}")
-            if result and result.result:
-                if isinstance(result.result, TaskStatusUpdateEvent):
-                    # print(f"收到状态更新TaskStatusUpdateEvent",result.result)
-                    if result.result and result.result.status.message and result.result.status.message.parts:
-                        type = result.result.status.message.parts[0].type
-                        if type == "text":
-                            text = result.result.status.message.parts[0].text
-                            # print(f"TaskStatusUpdateEvent,Text内容: {text}")
-                            if not text:
-                                # 去掉空的思考
-                                continue
-                            yield {
-                                "type": "reasoning", "reasoning": text
-                            }
-                        elif type == "data":
-                            data = result.result.status.message.parts[0].data
-                            # print(f"TaskStatusUpdateEvent,data内容: {data}")
-                            yield {
-                                "type": "data", "data": data
-                            }
-                        else:
-                            print(f"收到的数据类型未知: {type}")
-                elif isinstance(result.result, TaskArtifactUpdateEvent):
-                    for part in result.result.artifact.parts:
-                        if isinstance(part, TextPart) and part.text:
-                            # print(f"TaskArtifactUpdateEvent,Text内容: {part.text}")
-                            yield {
-                                "type": "text", "text": part.text,
-                            }
-            else:
-                print(f"收到了其它消息内容，{result}")
-        print(f"任务结束，返回flag为done的信息。")
-        yield {
-            "type": "final", "text": "[DONE]",
-        }
+            if isinstance(result.root, JSONRPCErrorResponse):
+                print("Error: ", result.root.error)
+                return False, contextId, taskId
+            event = result.root.result
+            contextId = event.contextId
+            if isinstance(event, Task):
+                taskId = event.id
+            elif isinstance(event, TaskStatusUpdateEvent) or isinstance(
+                event, TaskArtifactUpdateEvent
+            ):
+                taskId = event.taskId
+            elif isinstance(event, Message):
+                message = event
+            print(f"stream event => {event.model_dump_json(exclude_none=True)}")
+        # Upon completion of the stream. Retrieve the full task if one was made.
+        if taskId:
+            taskResult = await client.get_task(
+                GetTaskRequest(
+                    id=str(uuid4()),
+                    params=TaskQueryParams(id=taskId),
+                )
+            )
+            taskResult = taskResult.root.result
+    else:
+        try:
+            # For non-streaming, assume the response is a task or message.
+            event = await client.send_message(
+                SendMessageRequest(
+                    id=str(uuid4()),
+                    params=payload,
+                )
+            )
+            event = event.root.result
+        except Exception as e:
+            print("Failed to complete the call", e)
+        if not contextId:
+            contextId = event.contextId
+        if isinstance(event, Task):
+            if not taskId:
+                taskId = event.id
+            taskResult = event
+        elif isinstance(event, Message):
+            message = event
 
-    async def _complete_task(self, prompt: str, task_id: str):
-        message = {
-            "role": "user",
-            "parts": [
-                {
-                    "type": "text",
-                    "text": prompt,
-                }
-            ]
-        }
-
-        payload = {
-            "id": task_id,
-            "sessionId": self.session_id,
-            "acceptedOutputModes": ["text"],
-            "message": message,
-        }
-
-        if self.use_push_notifications:
-            payload["pushNotification"] = {
-                "url": f"http://{self.notification_receiver_host}:{self.notification_receiver_port}/notify",
-                "authentication": {
-                    "schemes": ["bearer"],
+    if message:
+        print(f"\n{message.model_dump_json(exclude_none=True)}")
+        return True, contextId, taskId
+    if taskResult:
+        # Don't print the contents of a file.
+        task_content = taskResult.model_dump_json(
+            exclude={
+                "history": {
+                    "__all__": {
+                        "parts": {
+                            "__all__": {"file"},
+                        },
+                    },
                 },
-            }
-
-        if self.streaming:
-            response_stream = self.client.send_task_streaming(payload)
-            async for result in response_stream:
-                print(f"stream event => {result.model_dump_json(exclude_none=True)}")
-            task_result = await self.client.get_task({"id": task_id})
-        else:
-            task_result = await self.client.send_task(payload)
-            print(f"\n{task_result.model_dump_json(exclude_none=True)}")
-
-        state = TaskState(task_result.result.status.state)
-        if state.name == TaskState.INPUT_REQUIRED.name:
-            # 递归重新调用直到任务完成
-            return await self._complete_task(prompt, task_id)
-        else:
-            return True
+            },
+            exclude_none=True,
+        )
+        print(f"\n{task_content}")
+        ## if the result is that more input is required, loop again.
+        state = TaskState(taskResult.status.state)
+        if state.name == TaskState.input_required.name:
+            return (
+                await completeTask(
+                    client,
+                    streaming,
+                    use_push_notifications,
+                    notification_receiver_host,
+                    notification_receiver_port,
+                    taskId,
+                    contextId,
+                ),
+                contextId,
+                taskId,
+            )
+        ## task is complete
+        return True, contextId, taskId
+    ## Failure case, shouldn't reach
+    return True, contextId, taskId
 
 
 # 作为脚本运行时调用
 if __name__ == "__main__":
     async def main():
-        session_id = time.strftime("%Y%m%d%H%M%S",time.localtime())
-        wrapper = A2AClientWrapper(session_id=session_id, agent_url="http://localhost:10005")
-        await wrapper.run("北京的今天的天气是?")
+        agent = "http://localhost:10005"
+        session = 0
+        history = False
+        use_push_notifications = False
+        push_notification_receiver =  "http://localhost:5000"
+        header  = [
+            "X-A2A-Client-Name=Python-Client",
+            "X-A2A-Client-Version=1.0.0",
+        ]
+        prompt = "北京的今天的天气是?"
+        file_path = ""
+        await cli(agent, session, history, use_push_notifications, push_notification_receiver, header,  prompt, file_path)
     asyncio.run(main())

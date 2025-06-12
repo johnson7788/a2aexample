17a0e8ef6430299d1d86b297a073acb7b3a2d45f
Johnson <linghuchongxajh@qq.com>
2025-06-12 22:51:07 +0800

    2025-06-12_22:51

--- SomeExample/a2a_client.py
+++ SomeExample/a2a_client.py
@@ -2,14 +2,13 @@ import uuid
 import httpx
 from a2a.client import A2AClient
 import asyncio
-from a2a.types import (MessageSendParams, SendMessageRequest)
-
+from a2a.types import (MessageSendParams, SendMessageRequest, SendStreamingMessageRequest)
 
 async def httpx_client():
     async with httpx.AsyncClient() as httpx_client:
         # 初始化客户端（确保base_url包含协议头）
 
-        http_client = await A2AClient.get_client_from_agent_card_url(
+        client = await A2AClient.get_client_from_agent_card_url(
             httpx_client, 'http://localhost:9000'  # 确保此处是完整 URL
         )
 
@@ -24,19 +23,25 @@ async def httpx_client():
                 'messageId': request_id
             }
         }
-
+        # 非流的形式
         # 创建请求对象
         request = SendMessageRequest(
             id=request_id,
             params=MessageSendParams(**send_message_payload) )
 
         # 发送请求
-        response = await http_client.send_message(request)
-        print(response)
+        response = await client.send_message(request)
+        print(response.model_dump(mode='json', exclude_none=True))
 
+        # 流式请求的示例
+        # 创建请求对象
+        streaming_request = SendStreamingMessageRequest(
+            params=MessageSendParams(**send_message_payload)  # 同样的 payload 可以用于非流式请求
+        )
 
+        stream_response = client.send_message_streaming(streaming_request)
+        async for chunk in stream_response:
+            print(chunk.model_dump(mode='json', exclude_none=True))
 
-
-asyncio.run(httpx_client())
-
-#https://www.a2aprotocol.net/zh/docs/a2a-python-sdk-basic
+if __name__ == '__main__':
+    asyncio.run(httpx_client())
--- SomeExample/a2a_server.py
+++ SomeExample/a2a_server.py
@@ -1,8 +1,22 @@
-from pydoc import describe
-from typing import override
+import uvicorn
+from typing_extensions import override
+from typing import Any, Literal, List
 from a2a.types import AgentCard,AgentCapabilities
-
+from a2a.server.agent_execution import AgentExecutor,RequestContext
+from a2a.server.events import EventQueue
+from a2a.utils import new_agent_text_message, new_task, new_text_artifact
 from a2a.types import AgentSkill
+from a2a.server.apps import A2AStarletteApplication
+from a2a.server.request_handlers import DefaultRequestHandler
+from a2a.server.tasks import InMemoryTaskStore
+from collections.abc import AsyncIterable
+from a2a.types import (
+    TaskArtifactUpdateEvent,
+    TaskState,
+    TaskStatus,
+    TaskStatusUpdateEvent,
+)
+
 skill = AgentSkill(
     id="hello-world",
     name="Hello World Skill",
@@ -14,11 +28,11 @@ skill = AgentSkill(
 agent_card = AgentCard(
     name="Hello World Agent",
     description="Just a hello world agent",
-    url="http:/0.0.0.0:9000",
+    url="http://localhost:9000",
     version="0.0.1",
     defaultInputModes=["text"],
     defaultOutputModes=["text"],
-    capabilities=AgentCapabilities(streams=True, tools=True),
+    capabilities=AgentCapabilities(streaming=True, tools=True),
     skills=[skill],
 )
 
@@ -28,10 +42,21 @@ class HelloWorldAgent:
     def invoke(self):
         return "hello world"
 
+    async def stream(self, query: str, sessionId: str) -> AsyncIterable[Any]:
+        """Process a query with streaming responses.
 
-from a2a.server.agent_execution import AgentExecutor,RequestContext
-from a2a.server.events import EventQueue
-from a2a.utils import new_agent_text_message
+        Args:
+            query: The user's query text
+            sessionId: A unique session identifier
+
+        Yields:
+            Formatted response chunks with status updates
+        """
+        # Save context per session
+        words = ["Hello", "World", "!", "How", "are", "you" , "!"]
+        for word in words:
+            yield {"is_task_complete": False, "content": word}
+        yield {"is_task_complete": True, "content": ""}
 
 class HelloWorldAgentExecutor(AgentExecutor):
     "Test agent executor"
@@ -42,37 +67,71 @@ class HelloWorldAgentExecutor(AgentExecutor):
     async def execute(self, context: RequestContext, event_queue: EventQueue) -> None:
         "execute the agent"
         result = self.agent.invoke()
-        await event_queue.enqueue_event(new_agent_text_message(result))   
+        event_queue.enqueue_event(new_agent_text_message(result))
 
+    # 其它示例，请求其它应用时
+    # @override
+    # async def execute(self, context: RequestContext, event_queue: EventQueue) -> None:
+    #     "execute the agent"
+    #     query = context.get_user_input()
+    #     task = context.current_task
+    #     print(f"query: {query}, task: {task}")
+    #     async for item in self.agent.stream(query, task.contextId):
+    #         is_task_complete = item['is_task_complete']
+    #         content = item['content']
+    #         if not is_task_complete:
+    #             event_queue.enqueue_event(
+    #                 TaskStatusUpdateEvent(
+    #                     status=TaskStatus(
+    #                         state=TaskState.working,
+    #                         message=new_agent_text_message(
+    #                             content,
+    #                             task.contextId,
+    #                             task.id,
+    #                         ),
+    #                     ),
+    #                     final=False,
+    #                     contextId=task.contextId,
+    #                     taskId=task.id,
+    #                 )
+    #             )
+    #         else:
+    #             event_queue.enqueue_event(
+    #                 TaskArtifactUpdateEvent(
+    #                     append=False,
+    #                     contextId=task.contextId,
+    #                     taskId=task.id,
+    #                     lastChunk=True,
+    #                     artifact=new_text_artifact(
+    #                         name='current_result',
+    #                         description='Result of request to agent.',
+    #                         text=content,
+    #                     ),
+    #                 )
+    #             )
+    #             event_queue.enqueue_event(
+    #                 TaskStatusUpdateEvent(
+    #                     status=TaskStatus(state=TaskState.completed),
+    #                     final=True,
+    #                     contextId=task.contextId,
+    #                     taskId=task.id,
+    #                 )
+    #             )
     @override
     async def cancel(self, context: RequestContext, event_queue: EventQueue) -> None:
         raise NotImplementedError("HelloWorldAgentExecutor does not support cancel")
 
+#定义处理器请求
+request_handler = DefaultRequestHandler(
+    agent_executor=HelloWorldAgentExecutor(),
+    task_store=InMemoryTaskStore(),  #用于任务状态的管理
 
-from a2a.server.apps import A2AStarletteApplication
-from a2a.server.request_handlers import DefaultRequestHandler
-from a2a.server.tasks import InMemoryTaskStore
+)
 
-
+#构建服务器
+server = A2AStarletteApplication(
+    agent_card=agent_card,http_handler=request_handler
+)
 
 if __name__ == '__main__':
-    #1.定义技能
-    skill = skill
-
-    #2.定义智能体名片
-    agent_card = agent_card
-
-    #定义处理器请求
-    request_handler = DefaultRequestHandler(
-        agent_executor=HelloWorldAgentExecutor(),
-        task_store=InMemoryTaskStore(),  #用于任务状态的管理
-
-    )
-
-    #构建服务器
-    server = A2AStarletteApplication(
-        agent_card=agent_card,http_handler=request_handler
-    )
-
-    import uvicorn
     uvicorn.run(server.build(),host='0.0.0.0', port=9000)
